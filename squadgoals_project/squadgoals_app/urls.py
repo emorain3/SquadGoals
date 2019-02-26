bdb908c83d8056dfdd5dae9f93533b3c3dc46fe3
ecclesia-morain <emorain@live.com>
2019-02-26 01:05:51 -0500

    created full CRUD routes...

--- squadgoals_project/squadgoals_app/urls.py
+++ squadgoals_project/squadgoals_app/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import *
+from rest_framework import routers
+from .api import *
+# from .views import *
+
+
+router = routers.DefaultRouter()
+router.register('api/appuser', AppUserViewset)
+router.register('api/goal', GoalViewset)
+# How do I make this route CRUD descriptive? Where it takes in the id of a specific goal too?
+router.register('api/goal/subgoal', SubgoalViewset) 
 
 urlpatterns = [
-    path('', index)
+    path('', router.urls)
 ]
