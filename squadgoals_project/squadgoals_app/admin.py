0096319d9752839dce60518b2587b1bd0ddd9715
ecclesia-morain <emorain@live.com>
2019-02-27 03:12:44 -0500

    Scrapping Python and Django for backend. Replacing with MERN

--- squadgoals_project/squadgoals_app/admin.py
+++ squadgoals_project/squadgoals_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from  .models import AppUser
+from  .models import *
 
 
 # Register your models here.
 admin.site.register(AppUser)
+admin.site.register(Goal)
+admin.site.register(Subgoal)
--- squadgoals_project/squadgoals_app/urls.py
+++ squadgoals_project/squadgoals_app/urls.py
@@ -8,7 +8,7 @@ router = routers.DefaultRouter()
 router.register('api/appuser', AppUserViewset, 'appuser')
 router.register('api/goal', GoalViewset, 'goal')
 # How do I make this route CRUD descriptive? Where it takes in the id of a specific goal too?
-router.register('api/subgoal', SubgoalViewset, 'subgoal') 
+router.register('api/goal/<int:id>/subgoal', SubgoalViewset, 'subgoal') 
 
 urlpatterns = router.urls
 
